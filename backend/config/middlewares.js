fe509903544645b521283752052e99598e184cd2
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2018-10-29 15:19:23 -0300

    Incluiding bodyParser and cors as middlewares to start the apis creation.

---
+++ backend/config/middlewares.js
@@ -0,0 +1,6 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+module.exports = app => {
+    app.use(bodyParser.json()).use(cors())
+}
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,7 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const consign = require('consign')
+
+consign().then('./config/middlewares.js').into(app)
 
 app.listen(3001)
 
-app.get('/checklists/:id', (req, resp) => {
-    resp.json({
-        id: req.params.id,
-        name: "Scrum Checklist",
-        items: [
-            {
-                id: 1,
-                description: "The team delivers tested software to every sprint",
-                section: "Values, principles and fundamentals",
-                priority: 55
-            },
-            {
-                id: 2,
-                description: "The team delivers tested software to every sprint",
-                section: "Values, principles and fundamentals",
-                priority: 55
-            }
-        ]
-    })
-})
