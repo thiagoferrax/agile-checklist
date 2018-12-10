6682a8cd6607a8cb4060fec03280ef4ca3b66bb5
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2018-12-10 10:19:00 -0300

    Changing the tree icon and including a link in the node description.

--- frontend/src/common/tree/treeItem.jsx
+++ frontend/src/common/tree/treeItem.jsx
@@ -30,10 +30,17 @@ export default class TreeItem extends Component {
                     <div className="treeItem">
                         <If test={this.props.children}>
                             <a onClick={() => this.toggleIcon(this.state.node)}>
-                                <i className={`fa fa-angle-${this.state.hideChildren ? 'right' : 'down'} ml-2`}/>
+                                <i className={`icon ion-md-${this.state.hideChildren ? 'arrow-dropright' : 'arrow-dropdown'} ml-2`}/>
                             </a>
                         </If>
-                        <div className="treeItemDescription">{this.state.node.description}</div> 
+                        <If test={this.props.children}>
+                            <div className="treeItemDescription">
+                                <a onClick={() => this.toggleIcon(this.state.node)}>{this.state.node.description}</a>
+                            </div> 
+                        </If>
+                        <If test={!this.props.children}>
+                            <div className="treeItemDescription">{this.state.node.description}</div> 
+                        </If>
                         <SlideBar node={this.state.node} onChange={this.props.onChange} hideSlideBar={this.props.hideSlideBar}/>
                     </div>            
                 </div>
