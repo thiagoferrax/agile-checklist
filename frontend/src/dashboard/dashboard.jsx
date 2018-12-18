39bf03624a6561d1ef407e2e5aa223d2ad417ca9
thiagoferrax <thiagoferrax@gmail.com>
2018-12-18 07:55:15 -0300

    Creating the EvaluationBarChart component to encapsulate the logic to get the needed data structure for the BarChart component.

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -8,7 +8,7 @@ import Content from '../common/template/content'
 import InfoBox from '../common/widget/infoBox'
 import ProgressBox from '../common/widget/progressBox'
 import ProjectBox from '../common/widget/projectBox'
-import BarChart from '../common/chart/barChart'
+import EvaluationBarChart from '../common/chart/evaluationBarChart'
 
 import Row from '../common/layout/row'
 
@@ -18,80 +18,17 @@ class Dashboard extends Component {
         this.props.getSummary()
     }
 
-    getDataSet(datasets, checklistId) {
-        return datasets.filter(dataset => dataset.label == checklistId)
-    }
-
-    getChartColor(index) {
-        const colors =[
-        '#33c9dd',
-        '#c9dd33',
-        '#dd33c9',
-        '#dd4733',
-        '#33dd9c',
-        '#3374dd',
-        '#dd9c33',
-        '#113166']
-
-        if(index > colors.length) {
-            index -= colors.length
-        }
-        return colors[index]
-    }
-    
-    getBarChartData(evaluations, projectId) {
-        const projectEvaluations = evaluations.filter(evaluation => evaluation.projectId === projectId)
-
-        projectEvaluations.sort((e1, e2) => {
-            console.log('sort', e1.sprint - e2.sprint)
-            return e1.sprint - e2.sprint
-        })
-
-        let color = 0
-        const sprintChecklistMap = projectEvaluations.reduce((map, evaluation) => {
-            const sprint = 'Sprint ' + evaluation.sprint
-            const checklist = evaluation.checklistDescription
-            const score = evaluation.score
-            
-            if(!map.labels.includes(sprint)) {
-                map.labels.push(sprint)                             
-            }             
-                
-            const dataset = this.getDataSet(map.datasets, checklist)
-            const index = map.labels.indexOf(sprint)
-            if(dataset && dataset.length) {
-                dataset[0].data[index] = evaluation.score
-            } else {
-                let data = []                
-                data[index] = evaluation.score
-                map.datasets.push({
-                    label: checklist,
-                    data,
-                    backgroundColor: this.getChartColor(color++)
-                })
-            }
-
-            return map
-        }, {labels:[], datasets:[]})
-
-        return sprintChecklistMap
-    }
-
     renderProjects() {
         const { projects, evaluations } = this.props.summary
 
         return projects.map(
-            project => {
-                const barChartData = this.getBarChartData(evaluations, project.id)
-
-                return (<ProjectBox key={project.id} cols='12' color='default' project={project.name}>
+            project => (
+                <ProjectBox key={project.id} cols='12' color='default' project={project.name}>
                     <Row>
-                        <BarChart cols='12 6' data={barChartData}/>
+                        <EvaluationBarChart cols='12 6' evaluations={evaluations} project={project} />
                     </Row>
                 </ProjectBox>)
-            }
-
-        )
+            )
     }
 
     render() {
