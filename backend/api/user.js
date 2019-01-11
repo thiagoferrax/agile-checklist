7ee47ffd330876be3c7938dd43e571dd5eb3411e
thiagoferrax <thiagoferrax@gmail.com>
2019-01-11 13:44:35 -0300

    Fixing the signup, showing the new user after adding it in the database.

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcrypt-nodejs')
+const jwt = require('jwt-simple')
+const { authSecret } = require('../.env')
 
 module.exports = app => {
     const obterHash = (password, callback) => {
@@ -8,13 +10,21 @@ module.exports = app => {
     }
 
     const save = (req, res) => {
+        console.log('req.body', req.body)
         obterHash(req.body.password, hash => {
             const password = hash
 
             app.db('users')
                 .insert({ name: req.body.name, email: req.body.email, password })
-                .then(_ => res.status(204).send())
-                .catch(err => res.status(400).json(err))
+                .returning('id')
+                .then(userId => {
+                    const payload = { id: userId[0] }
+                    res.json({
+                        name: req.body.name,
+                        email: req.body.email,
+                        token: jwt.encode(payload, authSecret),
+                    })
+                }).catch(err => res.status(400).json(err))
         })
     }
 
