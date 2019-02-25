848007441ced476fa8694b109bd81066fcb101ee
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2019-02-25 10:51:56 -0300

    After more tests and fixes, project deletion is working fine.

--- backend/api/project.js
+++ backend/api/project.js
@@ -1,55 +1,55 @@
 module.exports = app => {
-    const {existsOrError, notExistsOrError} = app.api.validation
-    
+    const { existsOrError, notExistsOrError } = app.api.validation
+
     const save = (req, res) => {
         const project = {
             id: req.body.id,
             name: req.body.name,
             team: req.body.team,
             userId: req.decoded.id
-        }        
+        }
 
-        if(req.params.id) project.id = req.params.id
+        if (req.params.id) project.id = req.params.id
 
-        try{
+        try {
             existsOrError(project.name, 'Name was not informed!')
             existsOrError(project.userId, 'User was not informed!')
             existsOrError(project.team, 'Team was not informed!')
         } catch (msg) {
-            return res.status(400).json({errors: [msg]})
-        }        
-        
+            return res.status(400).json({ errors: [msg] })
+        }
+
         const team = project.team
         delete project.team
 
-        if(project.id) {            
+        if (project.id) {
             app.db('projects')
                 .update(project)
-                .where({id: project.id})
+                .where({ id: project.id })
                 .then(_ => {
-                    if(team && team.length > 0) {
+                    if (team && team.length > 0) {
                         updateTeam(project.id, team, res)
                     } else {
                         res.status(204).send()
                     }
                 })
-                .catch(err => res.status(500).json({errors: [err]}))     
+                .catch(err => res.status(500).json({ errors: [err] }))
         } else {
             app.db('projects')
                 .insert(project)
                 .returning('id')
-            .then(projectId => insertTeam(projectId[0], team, res))
-                .catch(err => res.status(500).json({errors: [err]}))
+                .then(projectId => insertTeam(projectId[0], team, res))
+                .catch(err => res.status(500).json({ errors: [err] }))
         }
     }
 
     const updateTeam = (projectId, team, res) => {
         app.db('teams').where({ projectId: projectId }).del().then(
-            rowsDeleted => {               
-                insertTeam(projectId, team, res)            
-            }    
-        )           
-    }    
+            rowsDeleted => {
+                insertTeam(projectId, team, res)
+            }
+        )
+    }
 
     const insertTeam = (projectId, team, res) => {
         const rows = getTeamToInsert(projectId, team)
@@ -62,7 +62,7 @@ module.exports = app => {
 
     const getTeamToInsert = (projectId, team, initialTeam = []) => {
         return team.reduce((users, userId) => {
-            users.push({projectId, userId})
+            users.push({ projectId, userId })
             return users
         }, initialTeam)
     }
@@ -70,24 +70,22 @@ module.exports = app => {
     const remove = (req, res) => {
         const projectId = req.params.id
 
-        try{
+        try {
             existsOrError(projectId, "Project id was not informed!")
 
             app.db('teams').where({ projectId }).del().then(
-                teamDeleted => {   
-                    
-                    const rowsDeleted = app.db('projects').where({ id: projectId }).del()
-                    
-                    existsOrError(rowsDeleted, "Project was not found!")
-                    res.status(204).send()
-                }    
-            )
+                teamDeleted => {
+                    app.db('projects').where({ id: projectId }).del().then(rowsDeleted => {
+                        existsOrError(rowsDeleted, "Project was not found!")
+                        res.status(204).send()
+                    })
+                })
         } catch (msg) {
-            res.status(400).json({errors: [msg]})
+            res.status(400).json({ errors: [msg] })
         }
     }
 
-    const get = (req, res) => {        
+    const get = (req, res) => {
         const userId = req.decoded.id
 
         app.db.select(
@@ -107,9 +105,9 @@ module.exports = app => {
                     return map
                 }, {})
 
-                res.json(Object.values(projectsMap))          
+                res.json(Object.values(projectsMap))
             })
-            .catch(err => res.status(500).json({ errors: [err] }))       
+            .catch(err => res.status(500).json({ errors: [err] }))
     }
 
     const getById = (req, res) => {
@@ -125,22 +123,22 @@ module.exports = app => {
             .leftJoin('users', 'teams.userId', 'users.id')
             .where({ 'projects.id': req.params.id })
             .then(projectTeam => {
-                    let project = {
-                            id: projectTeam[0].id,
-                            name: projectTeam[0].name,
-                            userId: projectTeam[0].userId,
-                            team: []
-                        }
+                let project = {
+                    id: projectTeam[0].id,
+                    name: projectTeam[0].name,
+                    userId: projectTeam[0].userId,
+                    team: []
+                }
 
-                    project.team = projectTeam.reduce((team, member) => {
-                        team.push(member.memberId)     
-                        return team
-                    }, [])
+                project.team = projectTeam.reduce((team, member) => {
+                    team.push(member.memberId)
+                    return team
+                }, [])
 
-                    res.json(project)
-                })
+                res.json(project)
+            })
             .catch(err => res.status(500).json({ errors: [err] }))
     }
 
-    return {save, remove, get, getById}
+    return { save, remove, get, getById }
 }
