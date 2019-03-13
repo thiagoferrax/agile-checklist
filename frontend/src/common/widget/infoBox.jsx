fb1b7fb994e7dc73bbdd96192e6bb53a82b419d6
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2019-03-13 16:06:13 -0300

    Changing the dashboard main icons.

--- frontend/src/common/widget/infoBox.jsx
+++ frontend/src/common/widget/infoBox.jsx
@@ -5,7 +5,7 @@ import './infoBox.css'
 export default props => (
     <Grid cols={props.cols}>            
         <div className="info-box">
-            <span className={`info-box-icon bg-${props.color}`}><i className={`icon ion-md-${props.icon}`}></i></span>
+            <span className={`info-box-icon bg-${props.color}`}><i className={`icon ion-ios-${props.icon}`}></i></span>
             <div className="info-box-content">
                 <span className="info-box-text">{props.text}</span>
                 <span className="info-box-number">{props.value}</span>
