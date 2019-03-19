deca2dc58448962a28723a8e1b7b91a1720fcc41
thiagoferrax <thiagoferrax@gmail.com>
2019-03-19 05:19:15 -0300

    Code refactoring, fixes and layout improvements.

--- frontend/src/common/chart/summaryChart.jsx
+++ frontend/src/common/chart/summaryChart.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { fadeIn } from 'react-animations'
 import Radium, { StyleRoot } from 'radium'
+import Grid from '../layout/grid'
+import If from '../operator/if'
+
 
 const INITIAL_STATE = { index: 0 }
 
@@ -10,11 +13,12 @@ export default class SummaryChart extends Component {
         this.state = INITIAL_STATE
     }
 
-    nextChecklist() {
+    nextChecklist(step) {
         let index = this.state.index
-        index++
+        index += step
+
         if (!this.props.summaryData[index]) {
-            index = 0
+            index = step > 0 ? 0 : this.props.summaryData.length - 1
         }
         this.setState({ index })
     }
@@ -36,55 +40,67 @@ export default class SummaryChart extends Component {
 
         const styles = {
             fadeIn: {
-                animation: 'x 1s',
+                animation: 'x 1.5s',
                 animationName: Radium.keyframes(fadeIn, 'fadeIn')
             }
         }
         return (
             <StyleRoot>
                 <div className="row" style={styles.fadeIn}>
-                    <div className="col-sm-2 col-xs-6">
-                        <div className="description-block border-right">
-                            <button type="button" className="btn_ btn-default_ nextBtn" title="Next checklist" onClick={() => this.nextChecklist()}><i className="icon ion-md-skip-forward"/></button>
+                    <Grid cols='6 2'>
+                        <div className="description-block border-right carousel-inner">
                             <h5 className="description-header">{this.props.summaryData[this.state.index].checklist}</h5>
                             <span className="description-text">CHECKLIST</span>
+
+                            <If test={this.props.summaryData.length > 1} >
+                                <a class="left carousel-control" href="javascript:;" onClick={() => this.nextChecklist(-1)} data-slide="prev">
+                                    <span class="icon ion-ios-arrow-back"></span>
+                                    <span class="icon ion-ios-arrow-back"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="right carousel-control" href="javascript:;" onClick={() => this.nextChecklist(1)} data-slide="next">
+                                    <span class="icon ion-ios-arrow-forward"></span>
+                                    <span class="icon ion-ios-arrow-forward"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </If>
                         </div>
-                    </div>
-                    <div className="col-sm-2 col-xs-6">
+                    </Grid>
+                    <Grid cols='6 2'>
                         <div className="description-block border-right">
                             <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].currentScore.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].currentScore.percentageDirection}`}></i> {this.props.summaryData[this.state.index].currentScore.percentage}%</span>
                             <h5 className="description-header">{this.props.summaryData[this.state.index].currentScore.value}</h5>
                             <span className="description-text">CURRENT SCORE</span>
                         </div>
-                    </div>
-                    <div className="col-sm-2 col-xs-6">
+                    </Grid>
+                    <Grid cols='6 2'>
                         <div className="description-block border-right">
                             <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].teamParticipation.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].teamParticipation.percentageDirection}`}></i> {this.props.summaryData[this.state.index].teamParticipation.percentage}%</span>
                             <h5 className="description-header">{`${this.props.summaryData[this.state.index].teamParticipation.value}%`}</h5>
                             <span className="description-text">TEAM PARTICIPATION</span>
                         </div>
-                    </div>
-                    <div className="col-sm-2 col-xs-6">
+                    </Grid>
+                    <Grid cols='6 2'>
                         <div className="description-block border-right">
                             <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].minimumScore.sprint}`}</span>
                             <h5 className="description-header">{this.props.summaryData[this.state.index].minimumScore.value}</h5>
-                            <span className="description-text">MINIMUM SCORE</span>
+                            <span className="description-text">MINIMUM</span>
                         </div>
-                    </div>
-                    <div className="col-sm-2 col-xs-6">
+                    </Grid>
+                    <Grid cols='6 2'>
                         <div className="description-block">
                             <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].maximumScore.sprint}`}</span>
                             <h5 className="description-header">{this.props.summaryData[this.state.index].maximumScore.value}</h5>
-                            <span className="description-text">MAXIMUM SCORE</span>
+                            <span className="description-text">MAXIMUM</span>
                         </div>
-                    </div>
-                    <div className="col-sm-2 col-xs-6">
+                    </Grid>
+                    <Grid cols='6 2'>
                         <div className="description-block border-right">
                             <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].totalAverage.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].totalAverage.percentageDirection}`}></i> {this.props.summaryData[this.state.index].totalAverage.percentage}%</span>
                             <h5 className="description-header">{this.props.summaryData[this.state.index].totalAverage.value}</h5>
                             <span className="description-text">TOTAL AVERAGE</span>
                         </div>
-                    </div>
+                    </Grid>
                 </div>
             </StyleRoot>
         )
