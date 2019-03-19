2b115b37ec0c1f119407ee0c19926bf8e2f9812c
thiagoferrax <thiagoferrax@gmail.com>
2019-03-19 01:34:05 -0300

    Adding a fadeIn in summary bar chart.

--- frontend/src/common/chart/summaryChart.jsx
+++ frontend/src/common/chart/summaryChart.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { fadeIn } from 'react-animations'
+import Radium, { StyleRoot } from 'radium'
 
 const INITIAL_STATE = { index: 0 }
 
@@ -32,51 +34,59 @@ export default class SummaryChart extends Component {
             return <React.Fragment></React.Fragment>
         }
 
+        const styles = {
+            fadeIn: {
+                animation: 'x 1s',
+                animationName: Radium.keyframes(fadeIn, 'fadeIn')
+            }
+        }
         return (
-            <div className="row">
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block border-right">
-                        <button type="button" className="btn_ btn-default_" title="Next Checklist" onClick={() => this.nextChecklist()}><i className="fa fa-chevron-right"></i><i className="fa fa-chevron-right"></i></button>
-                        <h5 className="description-header">{this.props.summaryData[this.state.index].checklist}</h5>
-                        <span className="description-text">CHECKLIST</span>
+            <StyleRoot>
+                <div className="row" style={styles.fadeIn}>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block border-right">
+                            <button type="button" className="btn_ btn-default_ nextBtn" title="Next checklist" onClick={() => this.nextChecklist()}><i className="icon ion-md-skip-forward"/></button>
+                            <h5 className="description-header">{this.props.summaryData[this.state.index].checklist}</h5>
+                            <span className="description-text">CHECKLIST</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block border-right">
+                            <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].currentScore.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].currentScore.percentageDirection}`}></i> {this.props.summaryData[this.state.index].currentScore.percentage}%</span>
+                            <h5 className="description-header">{this.props.summaryData[this.state.index].currentScore.value}</h5>
+                            <span className="description-text">CURRENT SCORE</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block border-right">
+                            <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].teamParticipation.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].teamParticipation.percentageDirection}`}></i> {this.props.summaryData[this.state.index].teamParticipation.percentage}%</span>
+                            <h5 className="description-header">{`${this.props.summaryData[this.state.index].teamParticipation.value}%`}</h5>
+                            <span className="description-text">TEAM PARTICIPATION</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block border-right">
+                            <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].minimumScore.sprint}`}</span>
+                            <h5 className="description-header">{this.props.summaryData[this.state.index].minimumScore.value}</h5>
+                            <span className="description-text">MINIMUM SCORE</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block">
+                            <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].maximumScore.sprint}`}</span>
+                            <h5 className="description-header">{this.props.summaryData[this.state.index].maximumScore.value}</h5>
+                            <span className="description-text">MAXIMUM SCORE</span>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 col-xs-6">
+                        <div className="description-block border-right">
+                            <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].totalAverage.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].totalAverage.percentageDirection}`}></i> {this.props.summaryData[this.state.index].totalAverage.percentage}%</span>
+                            <h5 className="description-header">{this.props.summaryData[this.state.index].totalAverage.value}</h5>
+                            <span className="description-text">TOTAL AVERAGE</span>
+                        </div>
                     </div>
                 </div>
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block border-right">
-                        <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].currentScore.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].currentScore.percentageDirection}`}></i> {this.props.summaryData[this.state.index].currentScore.percentage}%</span>
-                        <h5 className="description-header">{this.props.summaryData[this.state.index].currentScore.value}</h5>
-                        <span className="description-text">CURRENT SCORE</span>
-                    </div>
-                </div>
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block border-right">
-                        <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].teamParticipation.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].teamParticipation.percentageDirection}`}></i> {this.props.summaryData[this.state.index].teamParticipation.percentage}%</span>
-                        <h5 className="description-header">{`${this.props.summaryData[this.state.index].teamParticipation.value}%`}</h5>
-                        <span className="description-text">TEAM PARTICIPATION</span>
-                    </div>
-                </div>
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block border-right">
-                        <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].minimumScore.sprint}`}</span>
-                        <h5 className="description-header">{this.props.summaryData[this.state.index].minimumScore.value}</h5>
-                        <span className="description-text">MINIMUM SCORE</span>
-                    </div>
-                </div>
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block">
-                        <span className="description-percentage text-muted">{`Sprint ${this.props.summaryData[this.state.index].maximumScore.sprint}`}</span>
-                        <h5 className="description-header">{this.props.summaryData[this.state.index].maximumScore.value}</h5>
-                        <span className="description-text">MAXIMUM SCORE</span>
-                    </div>
-                </div>
-                <div className="col-sm-2 col-xs-6">
-                    <div className="description-block border-right">
-                        <span className={`description-percentage text-${this.getPercentageColor(this.props.summaryData[this.state.index].totalAverage.percentageDirection)}`}><i className={`fa fa-caret-${this.props.summaryData[this.state.index].totalAverage.percentageDirection}`}></i> {this.props.summaryData[this.state.index].totalAverage.percentage}%</span>
-                        <h5 className="description-header">{this.props.summaryData[this.state.index].totalAverage.value}</h5>
-                        <span className="description-text">TOTAL AVERAGE</span>
-                    </div>
-                </div>
-            </div>
+            </StyleRoot>
         )
     }
 }
