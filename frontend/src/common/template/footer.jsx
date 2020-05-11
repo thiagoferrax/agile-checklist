c3b9f338e1edabde357cb7b5b7619c2c289adcf1
thiagoferrax <thiagoferrax@gmail.com>
2020-05-11 17:10:43 +0100

    Adjusting main footer text.

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
     <footer className="main-footer">
-        <strong>Copyright © 2019 <a href='https://www.linkedin.com/in/thiagoferraz/' target='_blank'> Ferrax</a>.</strong> All rights
+        <strong>Copyright © 2020 <a href='https://www.linkedin.com/in/thiagoferraz/' target='_blank'> Thiago Ferraz</a>.</strong> All rights
         reserved.
     </footer>
 )
